46d664336f88fe05cfe0d121ab7e91c4437050c6
Darsh yadav <darsh.yadav@proton.me>
2026-01-02 18:37:49 +0530

    flake change

--- flake.nix
+++ flake.nix
@@ -1,53 +1,16 @@
 {
-  description = "Ambxst - An Axtremely customizable shell by Axenide";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+    quickshell.url = "github:quickshell-mirror/quickshell";
+    ambxst = {
+      url = "github:axenide/ambxst";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, quickshell, ... }:
-    let
-      ambxstLib = import ./nix/lib.nix { inherit nixpkgs; };
-    in {
-      nixosModules.default = import ./nix/modules;
-
-      packages = ambxstLib.forAllSystems (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-
-          lib = nixpkgs.lib;
-
-          Ambxst = import ./nix/packages {
-            inherit pkgs lib self system quickshell ambxstLib;
-          };
-        in {
-          default = Ambxst;
-          Ambxst = Ambxst;
-        }
-      );
-
-      devShells = ambxstLib.forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          Ambxst = self.packages.${system}.default;
-        in {
-          default = pkgs.mkShell {
-            packages = [ Ambxst ];
-            shellHook = ''
-              export QML2_IMPORT_PATH="${Ambxst}/lib/qt-6/qml:$QML2_IMPORT_PATH"
-              export QML_IMPORT_PATH="$QML2_IMPORT_PATH"
-              echo "Ambxst dev environment loaded."
-            '';
-          };
-        }
-      );
-    };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;}
+    (inputs.import-tree ./nix);
 }
--- nix/modules/default.nix
+++
@@ -1,8 +0,0 @@
-# NixOS module for Ambxst
-{ config, lib, ... }:
-
-{
-  config = lib.mkIf (!config.networking.networkmanager.enable) {
-    networking.networkmanager.enable = lib.mkDefault true;
-  };
-}
